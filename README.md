e7ed7788b0c6cfd66c9e44cfd2c065a6edc8aacc
Claudia2305 <claudia2305c@hotmail.com>
2025-02-25 14:17:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# InnleveringsOppgave-3---Mars-2025---CC
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Claudia2305/InnleveringsOppgave-3---Mars-2025---CC)
---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Oppgave 1:
+Lage 3 variabler, med de tre primær-datatypene.
+Variabel som heter userName
+Variabel som heter userAge
+Variabel som heter userPresent
+*/
+
+/*
+const = variabel nøkkelord
+userName = variabel navn
+= = er lik / lagres som
+"Julie" = string verdi
+; = full stopp / ferdig med linjen
+*/
+const userName = "Julie";
+
+const userAge = 16;
+
